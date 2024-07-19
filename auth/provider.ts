f82d49f4597519536c9abe134674de4952601bb3
silentwolf-dev <omariimanibrown421@gmail.com>
2024-07-19 13:48:05 -0400

    provideer

--- auth/provider.ts
+++ auth/provider.ts
@@ -1,10 +1,12 @@
 /** @format */
-interface ProviderConfig {}
 
 export class BaseProvider {
   name: string;
+  config: any;
 
-  constructor(name: string, config: ProviderConfig) {
+  
+  constructor(name: string, config: any) {
     this.name = name;
+    this.config = config;
   }
 }
